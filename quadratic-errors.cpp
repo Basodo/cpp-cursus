3dba6ef4e697202ecba70a8535b7c6afddde1512
Basodo <105501797+Basodo@users.noreply.github.com>
2023-01-29 12:14:48 +0100

    Update quadratic-errors.cpp

--- quadratic-errors.cpp
+++ quadratic-errors.cpp
@@ -1,52 +1,53 @@
 #include <iostream>
 #include <cmath>
+
+float x,x1,x2;
+float a;
+float b;
+float c;
+float solution;
+
 int main()
 {
 
-    try 
-    {
-        // do something
-    } catch (std::exception )
+    try {
+        std::cout << "Please enter the values of a, b, and c:";
+        std::cin >> a >> b >> c;
 
-    float x,x1,x2;
-    float a;
-    float b;
-    float c;
-    float solution;
+        if (std::cin.fail()) {
+            throw std::runtime_error("Malformed user input");
+        }
 
-    std::cout << "Please enter the values of a, b, and c:";
-    std::cin >> a >> b >> c;
+        if (a == 0){
+            throw std::runtime_error("a must not be zero");
+        }
 
-    if(a ,b ,c =! float){
-        
+        for (int i = 1; i <= 10; ++i);
+        {
+            if ((b * b - 4 * a * c) > 0)
+            {
+                x1 = (-b + sqrt((b*b) -  4*a*c))/(2*a);
+                x2 = (-b - sqrt((b*b) -  4*a*c))/(2*a);
+                std::cout << "There are 2 solutions." << std::endl;
+                std::cout << "The solutions are: " << x1 << " and "<< x2;
+            } 
+            else if ((b * b - 4 * a * c) == 0)
+            {
+                x = (-b + sqrt((b*b) -  4*a*c))/(2*a);
+                std::cout << "There is 1 solution." << std::endl;
+                std::cout << "The solution is: " << x;
+            }
+            else if (((b * b - 4 * a * c) < 0))
+            {
+                
+                std::cout << "There is no solution.";
+
+            }
+        }
+
+        return (0);
+    }catch (std::runtime_error error) {
+        std::cout << "An error occurred: " << error.what();
+        return 0;
     }
-	}
-
-	return (0);
-
-float calculations()
-{
-    for (int i = 1; i <= 10; ++i);
-	{
-		if ((b * b - 4 * a * c) > 0)
-        {
-            x1 = (-b + sqrt((b*b) -  4*a*c))/(2*a);
-            x2 = (-b - sqrt((b*b) -  4*a*c))/(2*a);
-            std::cout << "There are 2 solutions." << std::endl;
-            std::cout << "The solutions are: " << x1 << " and "<< x2;
-        } 
-		else if ((b * b - 4 * a * c) == 0)
-        {
-            x = (-b + sqrt((b*b) -  4*a*c))/(2*a);
-            std::cout << "There is 1 solution." << std::endl;
-            std::cout << "The solution is: " << x;
-        }
-		else if (((b * b - 4 * a * c) < 0))
-        {
-            
-            std::cout << "There is no solution.";
-
-        }
-
-}
 }
